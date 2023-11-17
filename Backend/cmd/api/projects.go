8cd2493b838c84aa822b91cb865346f2c3f96a53
0xAckerMan <joelkores1@gmail.com>
2023-11-17 23:32:28 +0300

    Implementing the errors abstraction

--- Backend/cmd/api/projects.go
+++ Backend/cmd/api/projects.go
@@ -10,9 +10,9 @@ import (
 
 func (app *Application) all_projects(w http.ResponseWriter, r *http.Request) {
 	message := "All projects lists"
-	err := app.writeJSON(w, http.StatusOK, message, nil)
+    err := app.writeJSON(w, http.StatusOK, envelope{"error": message}, nil)
 	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        app.serverErrorResponse(w, r, err)
 	}
 }
 
@@ -32,9 +32,9 @@ func (app *Application) single_project(w http.ResponseWriter, r *http.Request) {
 		Created_by:   1,
 		Created_date: time.Now(),
 		Due_date:     time.Now().Add(72 * time.Hour),
-		Done:         0,
+		Done:         false,
 	}
-	err = app.writeJSON(w, http.StatusOK, project, nil)
+    err = app.writeJSON(w, http.StatusOK, envelope{"project": project}, nil)
 }
 
 func (app *Application) pm_projects(w http.ResponseWriter, r *http.Request) {
@@ -42,13 +42,13 @@ func (app *Application) pm_projects(w http.ResponseWriter, r *http.Request) {
     if err != nil{
         http.NotFound(w,r)
     }
-    fmt.Fprintf(w, "all projects belonging to pmof id %d \n", id)
+    fmt.Fprintf(w, "all projects belonging to pm of id %d \n", id)
 }
 
 func (app *Application) employee_projects (w http.ResponseWriter, r *http.Request){
     id, err := app.readIDparam(r)
     if err != nil{
-        http.NotFound(w,r)
+        app.notFoundResponse(w, r)
     }
 
     fmt.Fprintf(w, "all employee projects of id %d \n", id)
@@ -57,7 +57,8 @@ func (app *Application) employee_projects (w http.ResponseWriter, r *http.Reques
 func (app *Application) employee_completed_projects(w http.ResponseWriter, r *http.Request){
     id,err := app.readIDparam(r)
     if err != nil {
-        http.NotFound(w, r)
+        app.notFoundResponse(w, r)
+        return
     }
     fmt.Fprintf(w, "Employee of id %d completed projects \n", id)
 }
@@ -65,7 +66,8 @@ func (app *Application) employee_completed_projects(w http.ResponseWriter, r *ht
 func (app *Application) mark_complete(w http.ResponseWriter, r *http.Request){
     id, err := app.readIDparam(r)
     if err != nil{
-        http.NotFound(w, r)
+        app.notFoundResponse(w, r)
+        return
     }
     fmt.Fprintf(w, "project %d marked as completed \n", id)
 }
@@ -73,20 +75,40 @@ func (app *Application) mark_complete(w http.ResponseWriter, r *http.Request){
 func (app *Application) mark_uncomplete(w http.ResponseWriter, r *http.Request){
     id,err := app.readIDparam(r)
     if err != nil {
-        http.NotFound(w, r)
+        app.notFoundResponse(w, r)
         return
     }
     fmt.Fprintf(w, "project of id %d marked uncomplete \n", id)
 }
 
 func (app *Application) create_project(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintln(w, "created a new project")
+    var input struct{
+        Name string `json:"name"`
+        Category []string `json:"category"`
+        Excerpt string `json:"excerpt"`
+        Description string `json:"description"`
+        Assigned_to int `json:"assigned_to"`
+        Created_by int `json:"created_by"`
+        Due_date time.Time `json:"due_date"`
+        Done bool `json:"done"`
+    }
+    err := app.readJSON(w, r, &input)
+    if err != nil {
+        app.badRequestResponse(w, r, err)
+        return
+    }
+    err = app.writeJSON(w,200, envelope{"message": input},nil)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+        return
+    }
 }
 
 func (app *Application) update_project (w http.ResponseWriter, r *http.Request){
     id, err := app.readIDparam(r)
     if err != nil{
-        http.NotFound(w, r)
+        app.notFoundResponse(w, r)
+        return
     }
     fmt.Fprintf(w, "update project of id %d \n", id)
 }
@@ -94,7 +116,8 @@ func (app *Application) update_project (w http.ResponseWriter, r *http.Request){
 func (app *Application) delete_project (w http.ResponseWriter, r *http.Request){
     id, err := app.readIDparam(r)
     if err != nil {
-        http.NotFound(w, r)
+        app.notFoundResponse(w, r)
+        return
     }
     fmt.Fprintf(w, "deleted project of id %d \n", id)
 }
