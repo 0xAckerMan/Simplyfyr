43fc41a0d91b312b9229ca0da5b11a7c27c22664
0xAckerMan <joelkores1@gmail.com>
2023-11-17 23:31:12 +0300

    Enveloping the healthchecker

--- Backend/cmd/api/healthcheck.go
+++ Backend/cmd/api/healthcheck.go
@@ -5,10 +5,12 @@ import (
 )
 
 func (app *Application) healthcheck (w http.ResponseWriter, r *http.Request){
-    status := map[string]string{
-        "status": "active",
-        "environment": app.config.env,
-        "version": Version,
+    status := envelope{
+        "system info" : map[string]string{
+            "status": "active",
+            "environment": app.config.env,
+            "version": Version,
+        },
     }
     err := app.writeJSON(w, http.StatusOK, status, nil)
     if err != nil {
