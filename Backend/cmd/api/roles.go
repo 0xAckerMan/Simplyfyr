dc18ad11a2ed57d6779b463206d101c71a90b339
0xAckerMan <joelkores1@gmail.com>
2023-12-06 23:47:21 +0300

    User roles

---
+++ Backend/cmd/api/roles.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/0xAckerMan/Simplifyr/Backend/internal/data"
+)
+
+func (app *Application) all_roles(w http.ResponseWriter, r *http.Request) {
+    var roles []*data.Role
+    roles, err := app.models.Roles.Get_all()
+    if err != nil {
+        app.serverErrorResponse(w,r,err)
+    }
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"roles": roles}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *Application) single_role(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDparam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+	}
+	role, err := app.models.Roles.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"role": role}, nil)
+}
+
+func (app *Application) create_role(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Role string `json:"role"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	role := &data.Role{
+		Role: input.Role,
+	}
+
+	err = app.models.Roles.Insert(role)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	header := make(http.Header)
+	header.Set("Location", fmt.Sprintf("/v1/roles/%d", role.Id))
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"role": role}, header)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
