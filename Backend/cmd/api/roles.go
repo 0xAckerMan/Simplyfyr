85e989d4520869c7aa0d3be787c545e3c4b09b32
0xAckerMan <joelkores1@gmail.com>
2023-12-08 22:43:10 +0300

    User roles update model

--- Backend/cmd/api/roles.go
+++ Backend/cmd/api/roles.go
@@ -4,16 +4,17 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/0xAckerMan/Simplifyr/Backend/internal/data"
 )
 
 func (app *Application) all_roles(w http.ResponseWriter, r *http.Request) {
-    var roles []*data.Role
-    roles, err := app.models.Roles.Get_all()
-    if err != nil {
-        app.serverErrorResponse(w,r,err)
-    }
+	var roles []*data.Role
+	roles, err := app.models.Roles.Get_all()
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"roles": roles}, nil)
 	if err != nil {
@@ -67,4 +68,45 @@ func (app *Application) create_role(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
+}
+
+func (app *Application) update_role(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDparam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	role, err := app.models.Roles.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+	}
+
+    var input struct{
+        Role *string `json:"role"`
+    }
+
+    err = app.readJSON(w,r,&input)
+    if err != nil {
+        app.badRequestResponse(w,r,err)
+        return
+    }
+
+    if input.Role != nil{
+        role.Role = *input.Role
+    }
+
+    err = app.models.Roles.Update(role)
+    if err != nil{
+        app.serverErrorResponse(w,r,err)
+        return
+    }
+
+    app.writeJSON(w,http.StatusOK,envelope{"role":role}, nil)
+
 }
