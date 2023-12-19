8079b4f9e2a35f1172eb5ade6c1c794bcb392a22
0xAckerMan <joelkores1@gmail.com>
2023-12-19 21:49:34 +0300

    conf user model to main model

--- Backend/internal/data/models.go
+++ Backend/internal/data/models.go
@@ -7,16 +7,19 @@ import (
 
 var(
     ErrRecordNotFound = errors.New("record not found")
+    ErrEditConflict= errors.New("edit conflict")
 )
 
 type Models struct{
     Projects ProjectModel
     Roles RoleModel
+    Users UserModel
 }
 
 func NewModel(db *sql.DB) Models{
     return Models{
         Projects: ProjectModel{DB: db},
         Roles: RoleModel{DB: db},
+        Users: UserModel{DB: db},
     }
 }
