2027cf14a732dbe899bac8596f761c788279e29b
0xAckerMan <joelkores1@gmail.com>
2024-01-08 09:17:26 +0300

    random commit

--- Backend/cmd/api/routes.go
+++ Backend/cmd/api/routes.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *Application) routes () *chi.Mux{
     r := chi.NewRouter()
+    r.Use(middleware.Logger)
     r.NotFound(app.notFoundResponse)
     r.MethodNotAllowed(app.methodNotAllowedResponse)
     r.Route("/v1", func(r chi.Router) {
--- Backend/go.mod
+++ Backend/go.mod
@@ -3,7 +3,7 @@ module github.com/0xAckerMan/Simplifyr/Backend
 go 1.21.4
 
 require (
-	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/chi/v5 v5.0.11
 	github.com/lib/pq v1.10.9
 )
 
